7d10372b53fb7829b16ad1e86e3399a57b7b2f22
Arpit Panwar <fakelove87@gmail.com>
2016-04-11 08:54:49 -0400

    Adding generate model and filter design files

---
+++ MovingWinFeats.m
@@ -0,0 +1,15 @@
+function [ featurevals ] = MovingWinFeats(x,fs,winLen,winDisp,featFn)
+    windowval = round(((1/fs)*length(x) - winLen+winDisp)/winDisp);
+    values_per_window = floor(length(x)/windowval);
+    start = 1;
+    values = cell(windowval,1);
+    i=1;
+    while windowval >0
+       values{i} = x(start:start+values_per_window);
+       start = start+values_per_window;
+       windowval = windowval-1;
+       i=i+1;
+    end
+   featurevals = cellfun(featFn,values);
+end
+
