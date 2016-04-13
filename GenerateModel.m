800c152b51ccdb607ae95be692865ed7e974afb2
Arpit Panwar <arpanwar@seas.upenn.edu>
2016-04-13 09:23:14 -0400

    Adding a stable prediction for subject 1 with correlation of 0.3480

--- GenerateModel.m
+++ GenerateModel.m
@@ -18,56 +18,88 @@ totalDuration = session_sub1_ecog.data.rawChannels(1).get_tsdetails.getDuration/
 
 sr = session_sub1_ecog.data.sampleRate;
 
+%% Predicting for subject 1
+
 traindata_ecog_sub1 = session_sub1_ecog.data.getvalues(1:round(sr*totalDuration),1:62);
 
-%% Generating features
 NumWins = @(xLen,fs,winLen,winDisp) ((1/fs)*xLen - winLen +winDisp)/winDisp;
 wins = NumWins(length(traindata_ecog_sub1),sr,0.1,0.05);
 featureMat = zeros([int64(wins),62*6]);
 
-% Average Time domain voltage and Frequency Domain voltage
-fft_sub1 = fft(traindata_ecog_sub1);
-magnitude_fft_sub1 = abs(fft_sub1);
+% Generating features
 
 for i=0:61
+    %Mean voltage in time domain
     curr = traindata_ecog_sub1(:,i+1);
     featureMat(:,i*6+1) = MovingWinFeats(curr,sr,0.1,0.05,@(x)mean(x));
     
-    curr = magnitude_fft_sub1(:,i+1);
-    num1 = FilterDesign_IIR(5,15);
-    featureMat(:,i*6+2) = MovingWinFeats(filtfilt(num1.coefficients{1},num1.coefficients{2},curr),sr,0.1,0.05,@(x)mean(x));
+    % Converting in frequency domain
+    [s,w] = spectrogram(curr,hamming(100),50,[],2e3,'yaxis');
     
-    num1 = FilterDesign_IIR(20,25);
-    featureMat(:,i*6+3) = MovingWinFeats(filtfilt(num1.coefficients{1},num1.coefficients{2},curr),sr,0.1,0.05,@(x)mean(x));
-
-    num1 = FilterDesign_IIR(75,115);
-    featureMat(:,i*6+4) = MovingWinFeats(filtfilt(num1.coefficients{1},num1.coefficients{2},curr),sr,0.1,0.05,@(x)mean(x));
-
-    num1 = FilterDesign_IIR(125,160);
-    featureMat(:,i*6+5) = MovingWinFeats(filtfilt(num1.coefficients{1},num1.coefficients{2},curr),sr,0.1,0.05,@(x)mean(x));
+    %5-15 hz frequency
+    num1 = find(w>=5 & w<15);
+    featureMat(1:length(s),i*6+2) =mean(abs(s(num1,:)),1)';
     
-    num1 = FilterDesign_IIR(160,175);
-    featureMat(:,i*6+6) = MovingWinFeats(filtfilt(num1.coefficients{1},num1.coefficients{2},curr),sr,0.1,0.05,@(x)mean(x));
+    %20-25 hz frequency
+    num1 = find(w>=20 & w<25);
+    featureMat(1:length(s),i*6+3) = mean(abs(s(num1,:)),1)';
+
+    %75-115 hz frequency
+    num1 = find(w>=75 & w<115);
+    featureMat(1:length(s),i*6+4) = mean(abs(s(num1,:)),1)';
+
+    %125-160 hz frequency
+    num1 = find(w>=125 & w<160);
+    featureMat(1:length(s),i*6+5) = mean(abs(s(num1,:)),1)';
+    
+    %160-175 hz frequency
+    num1 = find(w>=160 & w<175);
+    featureMat(1:length(s),i*6+6) = mean(abs(s(num1,:)),1)';
 
 end
 
 clearvars curr;
 
-trainlabels_sub_1= zeros([sr*totalDuration,5]);
+%Train labels
+trainlabels_sub_1 = session_sub1_dglov.data.getvalues(1:round(sr*totalDuration),1:5);
+
+%Decimate the training labels
+trainlabels_sub1_decimated = zeros([int64(length(trainlabels_sub_1)/50),5]);
 
 for i=1:5
-    trainlabels_sub_1(:,i) = session_sub1_dglov.data.getvalues(1:round(sr*totalDuration),i);
+    trainlabels_sub1_decimated(:,i) = decimate(trainlabels_sub_1(:,i),50);
 end
 
-trainlabels_sub1_decimated = decimate(trainlabels_sub_1,50);
+trainlabels_sub1_decimated = trainlabels_sub1_decimated(1:end-1,:);
 
-trainlabels_sub1_decimated = trainlabels_sub1_decimated(1:end-1);
+%Generating weight matrix
+weight_mat = (featureMat'*featureMat) \ (featureMat'*trainlabels_sub1_decimated); 
 
-weight_mat = inv(featureMat'*featureMat) * (featureMat'*trainlabels_sub1_decimated'); 
-
+%Predicting
 pred = featureMat*weight_mat;
 
 % Spline function takes in the time that y occured and what time y should
 % occur
+pred_splined = zeros([length(traindata_ecog_sub1),5]);
 
-pred_splined = spline((0:0.05:309.9),pred,(0:0.001:309.998));
+for i=1:5
+    pred_splined(:,i) = spline((0:0.05:309.9),pred(:,i),(0:0.001:309.998));
+end
+
+%Rounding
+pred_rounded = round(pred_splined);
+
+%Setting limits
+for i=1:5
+    minimum = min(trainlabels_sub_1(:,i));
+    pred_remove = find(pred_rounded < minimum);
+    pred_rounded(pred_remove) = minimum;
+    
+    maximum = max(trainlabels_sub_1(:,i));
+    pred_remove = find(pred_rounded > maximum);
+    pred_rounded(pred_remove) = maximum;
+    
+end
+
+%Checking the correlation
+correlation = mean(diag(corr(pred_rounded,trainlabels_sub_1)));
