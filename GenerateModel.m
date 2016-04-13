f0914e0afc55579d45fb88c20e6440a25a703971
Arpit Panwar <arpanwar@seas.upenn.edu>
2016-04-13 13:09:26 -0400

    Adding the first generated file and modified model generation

--- GenerateModel.m
+++ GenerateModel.m
@@ -30,21 +30,21 @@ trainlabels_sub_1 = session_sub1_dglov.data.getvalues(1:round(sr*totalDuration),
 
 weight_sub1 = LinearRegressionModel(traindata_ecog_sub1,trainlabels_sub_1,sr);
 
-%testdata_ecog_sub1 = session_sub1_test.data.getvalues(1:round(sr*testDuration),1:62);
+testdata_ecog_sub1 = session_sub1_test.data.getvalues(1:round(sr*testDuration),1:62);
 
 %Predicting
-pred_rounded_sub1 = Prediction_LinearReg(weight_sub1,trainlabels_sub_1,traindata_ecog_sub1,sr,totalDuration);   
+pred_rounded_sub1 = Prediction_LinearReg(weight_sub1,trainlabels_sub_1,testdata_ecog_sub1,sr,testDuration);   
 
 %Checking the correlation
-correlation_sub1 = mean(diag(corr(pred_rounded_sub1,trainlabels_sub_1)));
+%correlation_sub1 = mean(diag(corr(pred_rounded_sub1,trainlabels_sub_1)));
 
-clearvars traindata_ecog_sub1 trainlabels_sub_1;
+clearvars traindata_ecog_sub1 trainlabels_sub_1 testdata_ecog_sub1;
 
 %% Predicting for subject 2
 
 totalDuration = session_sub2_ecog.data.rawChannels(1).get_tsdetails.getDuration/10^6;
 
-%testDuration = session_sub2_test.data.rawChannels(1).get_tsdetails.getDuration/10^6;
+testDuration = session_sub2_test.data.rawChannels(1).get_tsdetails.getDuration/10^6;
 
 sr = session_sub2_ecog.data.sampleRate;
 
@@ -54,15 +54,15 @@ trainlabels_sub_2 = session_sub2_dglov.data.getvalues(1:round(sr*totalDuration),
 
 weight_sub2 = LinearRegressionModel(traindata_ecog_sub2,trainlabels_sub_2,sr);
 
-%testdata_ecog_sub2 = session_sub2_test.data.getvalues(1:round(sr*testDuration),1:62);
+testdata_ecog_sub2 = session_sub2_test.data.getvalues(1:round(sr*testDuration),1:48);
 
 %Predicting
-pred_rounded_sub2 = Prediction_LinearReg(weight_sub2,trainlabels_sub_2,traindata_ecog_sub2,sr,totalDuration);   
+pred_rounded_sub2 = Prediction_LinearReg(weight_sub2,trainlabels_sub_2,testdata_ecog_sub2,sr,testDuration);   
 
 %Checking the correlation
-correlation_sub2 = mean(diag(corr(pred_rounded_sub2,trainlabels_sub_2)));
+%correlation_sub2 = mean(diag(corr(pred_rounded_sub2,trainlabels_sub_2)));
 
-clearvars traindata_ecog_sub2 trainlabels_sub_2;
+clearvars traindata_ecog_sub2 trainlabels_sub_2 testdata_ecog_sub2;
 
 %% Predicting for subject 3
 
@@ -78,28 +78,27 @@ trainlabels_sub_3 = session_sub3_dglov.data.getvalues(1:round(sr*totalDuration),
 
 weight_sub3 = LinearRegressionModel(traindata_ecog_sub3,trainlabels_sub_3,sr);
 
-%testdata_ecog_sub3 = session_sub3_test.data.getvalues(1:round(sr*testDuration),1:62);
+testdata_ecog_sub3 = session_sub3_test.data.getvalues(1:round(sr*testDuration),1:64);
 
 %Predicting
-pred_rounded_sub3 = Prediction_LinearReg(weight_sub3,trainlabels_sub_3,traindata_ecog_sub3,sr,totalDuration);   
+pred_rounded_sub3 = Prediction_LinearReg(weight_sub3,trainlabels_sub_3,testdata_ecog_sub3,sr,testDuration);   
 
 %Checking the correlation
-correlation_sub3 = mean(diag(corr(pred_rounded_sub3,trainlabels_sub_3)));
+%correlation_sub3 = mean(diag(corr(pred_rounded_sub3,trainlabels_sub_3)));
 
-clearvars traindata_ecog_sub3 trainlabels_sub_3;
+clearvars traindata_ecog_sub3 trainlabels_sub_3 testdata_ecog_sub3;
 
 
 %% Average Correlation
 
-avg_corr = mean([correlation_sub1,correlation_sub2,correlation_sub3]);
+%avg_corr = mean([correlation_sub1,correlation_sub2,correlation_sub3]);
 
 %% Generating output cell matrix
 
-%{
+
 predicted_dg = cell(3,1);
 predicted_dg{1} = pred_rounded_sub1;
 predicted_dg{2} = pred_rounded_sub2;
 predicted_dg{3} = pred_rounded_sub3;
 
-save('LeaderBoardSubmission.mat',predicted_dg);
-%}
+save('LeaderBoardSubmission.mat','predicted_dg');
