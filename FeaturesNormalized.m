ced0fee496ff577d746a8779d4a8fe6e54766e6f
Justin Burrell <burrellj@med.upenn.edu>
2016-04-27 21:27:03 -0400

    no message

--- FeaturesNormalized.m
+++ FeaturesNormalized.m
@@ -1,3 +1,3 @@
-function = [features] = featuresNormalized(featureMat)
+function [features] = FeaturesNormalized(featureMat)
     features = bsxfun(@minus, featureMat, mean(featureMat)./(max(featureMat,2)-min(featureMat,2)));
 end
