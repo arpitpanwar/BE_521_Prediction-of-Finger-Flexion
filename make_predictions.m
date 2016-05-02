23a808aee6c82d0d764a4f401e987ad83998d855
Arpit Panwar <arpanwar@seas.upenn.edu>
2016-05-02 13:35:51 -0400

    Adding saved models and chosen features

--- make_predictions.m
+++ make_predictions.m
@@ -1,13 +1,13 @@
 function [predicted_dg] = make_predictions(test_ecog)
 
-% Inputs: test_ecog - 3 x 1 cell array containing ECoG for each subject, where test_ecog{i} 
+% Inputs: test_ecog - 3 x 1 cell array containing ECoG for each subject, where test_ecog{i}
 % to the ECoG for subject i. Each cell element contains a N x M testing ECoG,
 % where N is the number of samples and M is the number of EEG channels.
-% Outputs: predicted_dg - 3 x 1 cell array, where predicted_dg{i} contains the 
+% Outputs: predicted_dg - 3 x 1 cell array, where predicted_dg{i} contains the
 % data_glove prediction for subject i, which is an N x 5 matrix (for
 % fingers 1:5)
 
-% Run time: The script has to run less than 1 hour. 
+% Run time: The script has to run less than 1 hour.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -16,7 +16,7 @@ function [predicted_dg] = make_predictions(test_ecog)
 % Load Model. The variable models contains weights for each person,
 % each channel, and each finger.
 
-load 'SavedModels.mat';
+load 'SavedModels_10.mat';
 
 % Predict using linear predictor for each subject
 %create cell array with one element for each subject
@@ -25,39 +25,52 @@ history = 25;
 windowSize = 0.08;
 displ = 0.04;
 %for each subject
-for subj = 1:3 
+for subj = 1:3
     
     %get the testing ecog
-    testset = test_ecog{subj}; 
+    testset = test_ecog{subj};
     
     sr = 1000;
     
     duration = length(testset)/10^3;
+    
+    switch subj
+        case 1
+            testset = [testset(:,1:54),testset(:,56:end)];
+        case 2
+            testset=[testset(:,1:20),testset(:,22:37),testset(:,39:end)];
+        case 3
+            testset = testset;
+    end
     
     testset = Preprocess(testset);
     
     weight = weights_pred_ridreg{subj};
     
     load(strcat('Trainlimits_sub',num2str(subj),'.mat'));
-    load(strcat('chosenfeatures_sub',num2str(subj),'.mat'));
+    load(strcat('chosenfeatures_sub',num2str(subj),'.mat'));    
     
-    switch subj
-        case 1
-            history = 15;
-        case 2
-            history = 10;
-        case 3
-            history = 25;
-    end
+    predictions_ridge = Prediction_Ridge(weight,train_limits, testset,sr,duration,...
+        windowSize,displ,chosenFeatures,history,subj);
     
-    predictions = Prediction_Ridge(weight,train_limits, testset,sr,duration,...
-                    windowSize,displ,chosenFeatures,history,subj);
+%     load(strcat('chosenfeatures_logreg_sub',num2str(subj),'.mat'));
+%     
+%     predictions_logreg = Prediction_LogReg(weights_logreg{subj},train_limits, ...
+%             testset,sr,duration,windowSize,displ,chosenFeatures,history);
+%     
+%     load(strcat('chosenfeatures_ensemble_sub',num2str(subj),'.mat'));
+%     
+%     prediction_ensemble = Prediction_Ensemble(models_ensemble{subj},train_limits, ...
+%             testset,sr,duration,windowSize,displ,chosenFeatures,history,subj);
+%     
+%     pred_classifier = zeros(size(predictions_logreg));
+%     for i=1:5    
+%         pred_classifier(:,i) = mean([(predictions_logreg(:,i)),(prediction_ensemble(:,i))],2);
+%     end    
+        
+    predictions = predictions_ridge;%.*pred_classifier;
     
-    load(strcat('FilterWeights_sub',num2str(subj),'.mat'));
-    
-   % predictions = PostFilter(predictions,filterWeights);
-    
-    clearvars filterWeights train_limits weight chosenFeatures;
+    clearvars filterWeights train_limits weight  chosenFeatures;
     
     predicted_dg{subj} = predictions;
 end
